5c863ef93ddfd34b61766ffd74512dcc5541ab0e
Moussa-Kalla <moussa.kalla.am@gmail.com>
2025-02-22 17:29:05 +0100

    Updated

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,7 +1,10 @@
 import pandas as pd
 
 def load_data():
-    x_train = pd.read_csv("/Users/moussa-kalla/Challenge Data SNCF/data/raw/x_train_final.csv")
-    y_train = pd.read_csv("d/Users/moussa-kalla/Challenge Data SNCF/data/raw/y_train_final_j5KGWWK.csv")
-    x_test = pd.read_csv("/Users/moussa-kalla/Challenge Data SNCF/data/raw/x_test_final.csv")
+    x_train = pd.read_csv("data/raw/x_train_final.csv")
+    y_train = pd.read_csv("data/raw/y_train_final_j5KGWWK.csv")
+    x_test = pd.read_csv("data/raw/x_test_final.csv")
+    x_train = x_train.drop(columns=["Unnamed: 0", "Unnamed: 0.1"], errors='ignore')
+    x_test = x_test.drop(columns=["Unnamed: 0"], errors='ignore')
+    y_train = y_train.drop(columns=["Unnamed: 0"], errors='ignore')
     return x_train, y_train, x_test
